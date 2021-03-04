6c02d02ba31427c63ccedb6585caba026bd9aef0
MhdDaifullah <80031571+MhdDaifullah@users.noreply.github.com>
2021-03-04 16:06:51 +0700

    Kodingan_1
    kodingan untuk comit pertama

---
+++ script.js
@@ -0,0 +1,8 @@
+alert('program menginput angka positif dan negatif');
+let angka = prompt('input angka');
+
+if (angka >= 0) {
+  alert('angka ini positif');
+} else {
+  alert('angka ini negatif');
+}
